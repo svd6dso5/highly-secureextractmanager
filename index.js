2ca913b302ee5849bd0f3c749e9b9356cfef050f
svd6dso5 <izzetizzepsg@hotmail.com>
2024-05-13 16:51:34 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-function wiggleMaxLength(nums) {
-  if (nums.length === 0) return 0;
-  let up = 1;
-  let down = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) {
-      up = down + 1;
-    } else if (nums[i] < nums[i - 1]) {
-      down = up + 1;
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  for (let i = 0; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
     }
   }
-  return Math.max(up, down);
+  return Math.max(...dp);
 }
