9474b4ca1d83511e2ad206e514412b185e38f2e2
svd6dso5 <izzetizzepsg@hotmail.com>
2024-05-17 09:48:52 +0700

    test project

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function getIntersectionNode(headA, headB) {
-  let currA = headA;
-  let currB = headB;
-  while (currA !== currB) {
-    currA = currA ? currA.next : headB;
-    currB = currB ? currB.next : headA;
+const cocktailShakerSort = (arr) => {
+  let swapped = true;
+  let start = 0;
+  let end = arr.length - 1;
+  while (swapped) {
+    swapped = false;
+    for (let i = start; i < end; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+    swapped = false;
+    end--;
+    for (let i = end - 1; i >= start; i--) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    start++;
   }
-  return currA;
-}
+  return arr;
+};
